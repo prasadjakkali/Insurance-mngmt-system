2d319d0661dc58c52386d1d3234d1a151590a081
prasadjakkali <118116648+prasadjakkali@users.noreply.github.com>
2023-07-08 14:55:03 +0530

    Add files via upload

---
+++ insurancemanagement/connection.php
@@ -0,0 +1,6 @@
+<?php  
+		$servername = "localhost";  
+		$username = "root";  
+		$password = "";  
+		$conn = mysqli_connect($servername , $username , $password,"insurance") or die("unable to connect to host"); 
+?>   
---
+++ insurancemanagement/index.php
@@ -0,0 +1,16 @@
+<?php    
+
+?>
+<html>
+<head>
+<title>2022framework</title>
+</head>
+
+<frameset cols="30,70">
+
+<frame src="left.php" name="left">
+<frame src="default.php" name="right">
+</frameset>
+
+
+</html>
